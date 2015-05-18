a3900789a22778153f3fcb127e13d94ca36d5119
Pepijn de Vos <pepijndevos@gmail.com>
2015-05-18 12:54:42 +0200

    display stories

--- story/views.py
+++ story/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Story
+
+def index(request):
+    stories = Story.objects.all()
+    context = {'stories': stories}
+    return render(request, 'index.html', context)
+
+def detail(request, story_id):
+    question = get_object_or_404(Story, pk=story_id)
+    return render(request, 'story.html', {'story': question})
